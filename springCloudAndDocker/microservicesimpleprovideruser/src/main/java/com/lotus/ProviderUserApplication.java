01f67a372b8e27b11b4a2ac085b9bb7989160a80
7colorlotus <626477293@qq.com>
2017-05-31 23:15:06 +0800

    添加简单服务提供者

---
+++ springCloudAndDocker/microservicesimpleprovideruser/src/main/java/com/lotus/ProviderUserApplication.java
@@ -0,0 +1,14 @@
+package com.lotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/5/31.
+ */
+@SpringBootApplication
+public class ProviderUserApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ProviderUserApplication.class,args);
+    }
+}
