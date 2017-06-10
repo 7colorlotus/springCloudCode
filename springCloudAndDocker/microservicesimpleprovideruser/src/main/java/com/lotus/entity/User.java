f3d8048e21351ef5e52a3926217c22f009ecba4e
7colorlotus <626477293@qq.com>
2017-06-10 21:37:44 +0800

    添加Lombok应用

--- springCloudAndDocker/microservicesimpleprovideruser/src/main/java/com/lotus/entity/User.java
+++ springCloudAndDocker/microservicesimpleprovideruser/src/main/java/com/lotus/entity/User.java
@@ -1,5 +1,7 @@
 package com.lotus.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
@@ -7,6 +9,7 @@ import java.math.BigDecimal;
  * Created by Administrator on 2017/5/31.
  */
 @Entity
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,44 +22,4 @@ public class User {
     private Integer age;
     @Column
     private BigDecimal balance;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
 }
