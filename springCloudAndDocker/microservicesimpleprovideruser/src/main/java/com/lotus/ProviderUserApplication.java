5ff8cb740471f249fd3d5ccc51d8bbd09cec8d8a
7colorlotus <626477293@qq.com>
2017-06-18 14:58:55 +0800

    为provider和consumer添加注册客户端

--- springCloudAndDocker/microservicesimpleprovideruser/src/main/java/com/lotus/ProviderUserApplication.java
+++ springCloudAndDocker/microservicesimpleprovideruser/src/main/java/com/lotus/ProviderUserApplication.java
@@ -2,11 +2,13 @@ package com.lotus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Administrator on 2017/5/31.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderUserApplication {
     public static void main(String[] args){
         SpringApplication.run(ProviderUserApplication.class,args);
